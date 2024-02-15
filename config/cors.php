7e168add5f133d3e12e2b16d990b36475cb405f0
hiyoko <62996611+hiyoko9513@users.noreply.github.com>
2024-02-16 02:48:12 +0900

    :wrench: corsの設定

--- config/cors.php
+++ config/cors.php
@@ -17,11 +17,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/v1/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => explode(',', env('FRONT_APP_URL')),
 
     'allowed_origins_patterns' => [],
 
