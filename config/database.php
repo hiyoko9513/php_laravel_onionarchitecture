34800014f526f0daf11fdc16f9408ab18528baba
hiyoko <62996611+hiyoko9513@users.noreply.github.com>
2024-03-03 23:19:07 +0900

    :wrench: 読み込み書き込み用DBの設定

--- config/database.php
+++ config/database.php
@@ -47,12 +47,21 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'read' => [
+                'host' => env('DB_HOST_READ', env('DB_HOST', 'localhost')),
+                'port' => env('DB_PORT', '3306'),
+                'database' => env('DB_DATABASE', 'forge'),
+                'username' => env('DB_USERNAME', 'forge'),
+                'password' => env('DB_PASSWORD', ''),
+            ],
+            'write' => [
+                'host' => env('DB_HOST_WRITE', env('DB_HOST', 'localhost')),
+                'port' => env('DB_PORT', '3306'),
+                'database' => env('DB_DATABASE', 'forge'),
+                'username' => env('DB_USERNAME', 'forge'),
+                'password' => env('DB_PASSWORD', ''),
+            ],
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
